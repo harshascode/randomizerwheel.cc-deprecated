db7c89e0a250fea494f806d1c33fe32ec95f1169
Harsha <118063362+harshascode@users.noreply.github.com>
2024-01-25 09:49:51 -0800

    update the copyriht etc

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-10 max-md:flex-col">
-      <p>Copyright © 2023 Yuva Nidhi Scheme</p>
+      <p>Copyright © 2024 Randomizer Wheel</p>
       <div className="flex gap-x-9">
         <Link href="/terms">Terms & Conditions</Link>
         <Link href="/privacy-policy">Privacy Policy</Link>
