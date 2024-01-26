dbb06cb1e31b38c5b8a472699035b1f00d392a0d
Harsha <118063362+harshascode@users.noreply.github.com>
2024-01-26 05:27:03 -0800

    code updateed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,41 +1,34 @@
 "use client"
 
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
-interface IFooter {
- children: React.ReactNode;
-}
+const Footer: React.FC = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
-const Footer: React.FC<IFooter> = () => {
- const [content, setContent] = useState<React.ReactNode | null>(null);
-
- useEffect(() => {
+  useEffect(() => {
     const timer = setTimeout(() => {
-      setContent(
-        <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-10 max-md:flex-col">
-          <p>Copyright © 2024 Randomizer Wheel</p>
-          <div className="flex gap-x-9">
-            <Link href="/terms">Terms & Conditions</Link>
-            <Link href="/privacy-policy">Privacy Policy</Link>
-            <Link href="/contact-us">Contact</Link>
-            <Link href="/disclaimer">Disclaimer</Link>
-          </div>
-        </footer>
-      );
+      setIsMounted(true);
     }, 1000);
-
     return () => clearTimeout(timer);
- }, []);
+  }, []);
 
- return (
-    <div>
-      {content}
-    </div>
- );
+  return isMounted ? (
+    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-10 max-md:flex-col">
+      <p>Copyright © 2024 Randomizer Wheel</p>
+      <div className="flex gap-x-9">
+        <Link href="/terms">Terms & Conditions</Link>
+        <Link href="/privacy-policy">Privacy Policy</Link>
+        <Link href="/contact-us">Contact</Link>
+        <Link href="/disclaimer">Disclaimer</Link>
+      </div>
+    </footer>
+  ) : null;
 };
 
 export default Footer;
+
+
 
 
 // const Footer = () => {
